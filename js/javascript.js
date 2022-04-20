95f0303d959b4f9e65de8eeed9981a6023b8d7b5
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-20 11:48:50 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -11,6 +11,7 @@ osm.addTo(map)
 function popUPinfo(feature, layer) {
  layer.bindPopup(feature.properties.NIMI)
 }
+
 // add geoJSON polygons layer
 async function addDistrictsGeoJson(url) {
  const response = await fetch(url)
@@ -23,6 +24,7 @@ async function addDistrictsGeoJson(url) {
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
 
+// add geoJSON layer
 async function addCelltowersGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
@@ -30,28 +32,6 @@ async function addCelltowersGeoJson(url) {
  const clusters = L.markerClusterGroup()
  clusters.addLayer(markers)
  clusters.addTo(map)
-}
-addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-
-
-function createCircle(feature, latlng) {
- let options = {
- radius: 5,
- fillColor: 'red',
- fillOpacity: 0.5,
- color: 'red',
- weight: 1,
- opacity: 1,
- }
- return L.circleMarker(latlng, options)
-}
-async function addCelltowersGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const circles = L.geoJson(data, {
- pointToLayer: createCircle,
- })
- circles.addTo(map)
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
