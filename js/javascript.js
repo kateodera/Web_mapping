699c883cb10065c6060ac8ec497c83356b7df2f8
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-20 11:46:08 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -23,16 +23,15 @@ async function addDistrictsGeoJson(url) {
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
 
-
-// add geoJSON points layer*
 async function addCelltowersGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
  const markers = L.geoJson(data)
- markers.addTo(map)
+ const clusters = L.markerClusterGroup()
+ clusters.addLayer(markers)
+ clusters.addTo(map)
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-
 
 
 function createCircle(feature, latlng) {
