d4fd23296dc4b3607b1718aec1308ad532cc742a
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-20 11:31:29 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
+
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data, {
+ onEachFeature: popUPinfo,
+ })
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
