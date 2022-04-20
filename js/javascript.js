889558398f0888a02cd396dc3e7963558c210165
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-20 11:41:54 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -34,6 +34,28 @@ async function addCelltowersGeoJson(url) {
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 
+
+function createCircle(feature, latlng) {
+ let options = {
+ radius: 5,
+ fillColor: 'red',
+ fillOpacity: 0.5,
+ color: 'red',
+ weight: 1,
+ opacity: 1,
+ }
+ return L.circleMarker(latlng, options)
+}
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const circles = L.geoJson(data, {
+ pointToLayer: createCircle,
+ })
+ circles.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
 // default map settings
 function defaultMapSettings() {
  map.setView([58.373523, 26.716045], 12)
