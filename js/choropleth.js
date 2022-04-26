5a02d962eca4476422204ec24aaf9c4e40c2c89d
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-26 18:17:40 +0300

    Update choropleth.js

--- js/choropleth.js
+++ js/choropleth.js
@@ -1,1 +1,14 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+})
+osm.addTo(map)
+
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
 
