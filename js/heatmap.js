c44c99f754fe15b42712a46812fcae5f353d6714
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-26 17:01:43 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,3 +1,17 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+})
+osm.addTo(map)
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
+
+
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 // add geoJSON layer
 async function addGeoJson(url) {
@@ -18,4 +32,7 @@ function heatDataConvert(feature) {
 }
 
 
-
+// default map settings
+function defaultMapSettings() {
+ map.setView([58.373523, 26.716045], 12)
+}
