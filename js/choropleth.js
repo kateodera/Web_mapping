0078a3d38544ec6bff4f75cf10214f3d3738f755
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-27 11:02:40 +0300

    Update choropleth.js

--- js/choropleth.js
+++ js/choropleth.js
@@ -12,16 +12,27 @@ function popUPinfo(feature, layer) {
  layer.bindPopup(feature.properties.NIMI)
 }
 
-// add geoJSON polygons layer
-async function addDistrictsGeoJson(url) {
+addGeoJson('geojson/tartu_city_districts_edu.geojson')
+// add geoJSON layer
+async function addGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
- const polygons = L.geoJson(data, {
- onEachFeature: popUPinfo,
- })
- polygons.addTo(map)
+ L.choropleth(data, {
+ valueProperty: 'OBJECTID',
+ scale: ['#ffffff', '#ff9900'],
+ steps: 5,
+ mode: 'q', // q for quantile, e for equidistant
+ style: {
+ color: '#fff',
+ weight: 2,
+ fillOpacity: 0.8,
+ },
+ onEachFeature: function (feature, layer) {
+ layer.bindPopup('Value: ' + feature.properties.OBJECTID)
+ },
+ }).addTo(map)
 }
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
 
 
 // default map settings
