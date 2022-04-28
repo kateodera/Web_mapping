c20eb94cb75c983371093e65888189a289318162
Catherine Akinyi Odera <32383672+kateodera@users.noreply.github.com>
2022-04-28 18:28:05 +0300

    Update choropleth.js

--- js/choropleth.js
+++ js/choropleth.js
@@ -19,7 +19,7 @@ async function addGeoJson(url) {
  const data = await response.json()
  L.choropleth(data, {
  valueProperty: 'OBJECTID',
- scale: ['#ffffff', '#ff9900'],
+ scale: ['#fee0d2', '#de2d26'],
  steps: 5,
  mode: 'q', // q for quantile, e for equidistant
  style: {
